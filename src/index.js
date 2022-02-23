c22988f95868116f1d17de8db3fc58dd8a992fb7
Marina8520 <4079722@mail.ru>
2022-02-23 17:26:02 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = [],
+      close = [],
+      temp = [];
+  for( let arr of bracketsConfig) {
+    open.push(arr[0]);
+    close.push(arr[1]);
+  }
+  for(let el of str.split('')) {
+    for(let bracket of open) {
+      if(el === bracket) {
+        temp.push(el);
+      }
+    }
+    for(let bracket of close) {
+      if(el === bracket) {
+        temp.pop(el);
+      }
+    }
+  }
+  if(temp.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
